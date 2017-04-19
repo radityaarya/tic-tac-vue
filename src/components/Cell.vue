a375f27e7a7a78b4b640c3381ec50acc6cc163a7
Raditya <arya.raditya26@gmail.com>
2017-04-19 17:31:14 +0700

    cell style

---
+++ src/components/Cell.vue
@@ -0,0 +1,71 @@
+<template>
+
+</template>
+
+<script>
+    export default({
+      data(){
+
+      }
+    })
+</script>
+
+<style>
+    body{
+        background-color: #fff;
+        color: #fff;
+        font-family: 'Dosis', Helvetica, sans-serif;
+        -webkit-font-smoothing: antiasliased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        margin: 0px;
+    }
+    #app{
+        margin: 0 auto;
+        max-height: 270px;
+        color: #34495e;
+    }
+    h1{
+        text-transform: uppercase;
+        font-weight: bold;
+        font-size: 3em;
+    }
+    .restart{
+        background-color: #e74c3c;
+        color: #fff;
+        border: 0px;
+        border-bottom-left-radius: 10px;
+        border-bottom-right-radius: 10px;
+        font-family: 'Dosis', Helvetica, sans-serif;
+        font-size: 1.4em;
+        font-weight: bold;
+        margin: 0px;
+        padding: 15px;
+        width: 100%;
+    }
+    .restart:hover{
+        background-color: #c0392b;
+        cursor: pointer;
+    }
+    .scoreBoard{
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+        height: 15px;
+        background-color: #16a085;
+        box-shadow: 10px solid #fff;
+        padding: 20px;
+        overflow-x: none;
+    }
+    .scoreBoard h2{
+        margin: 0px;
+    }
+    .scoreBoard span{
+        float: right;
+        font-size: 1.5em;
+        font-weight: bold;
+        margin-left: 20px;
+    }
+</style>
